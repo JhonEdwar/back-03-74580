d92214bbda9fd9e6c6a3de08429073dfb810e73d
Jhon Edwar <jhonedwar192@gmail.com>
2025-03-15 00:46:14 -0500

    config

---
+++ src/routes/mocks.router.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+
+const router = Router();
+
+router.get('/mockingusers',);
+router.post('/mokingpets',);
+
+router.post('/generateData',);
+
+export default router;
